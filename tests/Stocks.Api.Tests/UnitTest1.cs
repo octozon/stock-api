ddb8dd77ffd30d63f8cb8dba0116cf51d79b26b9
sankovlex <sankovlex@gmail.com>
2021-10-06 19:47:25 +0300

    Init

--- tests/Stocks.Api.Tests/UnitTest1.cs
+++ tests/Stocks.Api.Tests/UnitTest1.cs
@@ -11,45 +11,45 @@ using Stocks.Application.Queries.Stocks;
 
 namespace Stocks.Api.Tests
 {
-    public class StocksControllerTests : StocksControllerTestsFixture
-    {
-        [Test]
-        public async Task Can_get_stocks()
-        {
-            /*// Act
-            HttpResponseMessage actual = await HttpClient.GetAsync("/stocks?page=1&size=30");
+//     public class StocksControllerTests : StocksControllerTestsFixture
+//     {
+//         [Test]
+//         public async Task Can_get_stocks()
+//         {
+//             /*// Act
+//             HttpResponseMessage actual = await HttpClient.GetAsync("/stocks?page=1&size=30");
+//
+//             // Assert
+//             actual.StatusCode.Should().Be(HttpStatusCode.OK);*/
+//         }
+//     }
 
-            // Assert
-            actual.StatusCode.Should().Be(HttpStatusCode.OK);*/
-        }
-    }
-
-    internal static class HttpExtensions
-    {
-        public static async Task<T> Result<T>(this HttpResponseMessage response) where T : class
-        {
-            string content = await GetContentAsync(response);
-
-            return JsonConvert.DeserializeObject<T>(content);
-        }
-
-        private static async Task<string> GetContentAsync(this HttpResponseMessage response)
-        {
-            string content = await response.Content.ReadAsStringAsync();
-
-            return content;
-        }
-    }
-
-    [TestFixture]
-    public abstract class StocksControllerTestsFixture : WebApplicationFactory<Startup>
-    {
-        [SetUp]
-        public void SetUp()
-        {
-            HttpClient = base.CreateClient();
-        }
-
-        public HttpClient HttpClient { get; set; }
-    }
+    // internal static class HttpExtensions
+    // {
+    //     public static async Task<T> Result<T>(this HttpResponseMessage response) where T : class
+    //     {
+    //         string content = await GetContentAsync(response);
+    //
+    //         return JsonConvert.DeserializeObject<T>(content);
+    //     }
+    //
+    //     private static async Task<string> GetContentAsync(this HttpResponseMessage response)
+    //     {
+    //         string content = await response.Content.ReadAsStringAsync();
+    //
+    //         return content;
+    //     }
+    // }
+    //
+    // [TestFixture]
+    // public abstract class StocksControllerTestsFixture : WebApplicationFactory<Startup>
+    // {
+    //     [SetUp]
+    //     public void SetUp()
+    //     {
+    //         HttpClient = base.CreateClient();
+    //     }
+    //
+    //     public HttpClient HttpClient { get; set; }
+    // }
 }
